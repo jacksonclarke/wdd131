bd88853546def5db6199bf7f6540db00be27b650
Jackson Clarke <104281508+jacksonclarke@users.noreply.github.com>
2026-01-14 09:47:38 -0700

    Mission W2 update image

--- mission/mission.js
+++ mission/mission.js
@@ -7,11 +7,11 @@ selectElem.addEventListener("change", changeTheme);
 function changeTheme() {
   let current = selectElem.value;
   if (current == "dark") {
-    logo.setAttribute("src", "/mission/images/byui-logo-white.png");
+    logo.setAttribute("src", "../mission/images/byui-logo-white.png");
     document.body.style.backgroundColor = "#333333";
     document.body.style.color = "#fff";
   } else {
-    logo.setAttribute("src", "/mission/images/byui-logo.webp");
+    logo.setAttribute("src", "../mission/images/byui-logo.webp");
     document.body.style.backgroundColor = "#fff";
     document.body.style.color = "#000";
   }
