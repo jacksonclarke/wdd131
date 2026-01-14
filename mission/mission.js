8d208fdc97a9c60529e6319a5a3d0847bd9824e0
Jackson Clarke <104281508+jacksonclarke@users.noreply.github.com>
2026-01-14 09:43:36 -0700

    Mission update week 2

---
+++ mission/mission.js
@@ -0,0 +1,18 @@
+let selectElem = document.querySelector("select");
+let logo = document.querySelector("img");
+let bodyContent = document.getElementById("page-content");
+
+selectElem.addEventListener("change", changeTheme);
+
+function changeTheme() {
+  let current = selectElem.value;
+  if (current == "dark") {
+    logo.setAttribute("src", "/mission/images/byui-logo-white.png");
+    document.body.style.backgroundColor = "#333333";
+    document.body.style.color = "#fff";
+  } else {
+    logo.setAttribute("src", "/mission/images/byui-logo.webp");
+    document.body.style.backgroundColor = "#fff";
+    document.body.style.color = "#000";
+  }
+}
